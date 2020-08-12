573284879be9cea94de35e45a402fd4ef4e31b5c
hhh2012aa <38385178+hhh2012aa@users.noreply.github.com>
2020-08-12 15:10:02 +0800

    Update and rename ga_tbd.py to ga_app.py

--- cdk/ga_app.py
+++ cdk/ga_app.py
@@ -1,42 +1,34 @@
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
-    aws_ecr as ecr,
     aws_ecs_patterns as ecs_patterns,
+    aws_globalaccelerator as globalaccelerator,
     core,
-    aws_elasticloadbalancingv2 as elbv2,
-    aws_globalaccelerator as globalaccelerator
 )
 
 
 class BonjourFargate(core.Stack):
 
-    def __init__(self, scope: core.Construct, id , **kwargs) -> None:
-        super().__init__(scope, id,**kwargs)
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
         # Create VPC and Fargate Cluster
         # NOTE: Limit AZs to avoid reaching resource quotas
-
-
-        
         vpc = ec2.Vpc(
             self, "MyVpc",
             max_azs=2
-        )        
-        
-        
+        )
+
         cluster = ecs.Cluster(
             self, 'Ec2Cluster',
             vpc=vpc
         )
-        
-        repo_ = ecr.Repository.from_repository_arn(self, id = 'hls-proxy', repository_arn='arn:aws:ecr:us-east-1:927529796467:repository/hls-proxy')
-        
+
         fargate_service = ecs_patterns.NetworkLoadBalancedFargateService(
             self, "FargateService",
             cluster=cluster,
             task_image_options={
-                'image': ecs.ContainerImage.from_ecr_repository(repository=repo_)
+                'image': ecs.ContainerImage.from_asset("../docker_folder")
             }
         )
 
@@ -46,20 +38,19 @@ class BonjourFargate(core.Stack):
             description="Allow http inbound from VPC"
         )
         
-
-        accelerator = globalaccelerator.Accelerator(self, "Accelerator")
-        listener1 = globalaccelerator.Listener(self, "Listener",accelerator = accelerator, port_ranges = [{"from_Port": 80,"to_Port": 80}])
         
-        endpoint_group = accelerator.EndpointGroup(self, "Group", listener=listener1)
+        accelerator = globalaccelerator.Accelerator(self, "Accelerator")
+        listener1 = globalaccelerator.Listener(self, "Listener",accelerator = accelerator, port_ranges = [{"fromPort": 80,"toPort": 80}])
+        
+        endpoint_group = globalaccelerator.EndpointGroup(self, "Group", listener=listener1)
         endpoint_group.add_load_balancer("NlbEndpoint", fargate_service.load_balancer)
+
 
         core.CfnOutput(
             self, "LoadBalancerDNS",
             value=fargate_service.load_balancer.load_balancer_dns_name
-        )
-        
+        ) 
 
 app = core.App()
-
-BonjourFargate(app, "Bonjour",env = core.Environment(account= '927529796467', region='ap-east-1'))
+BonjourFargate(app, "hls-hk",env = core.Environment(account= '927529796467', region='ap-east-1'))
 app.synth()
