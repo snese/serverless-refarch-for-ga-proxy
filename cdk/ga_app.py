feaad2517e2710d74d239422d8aa2f19d62b1e87
hhh2012aa <38385178+hhh2012aa@users.noreply.github.com>
2020-08-15 15:44:04 +0800

    Update ga_app.py

--- cdk/ga_app.py
+++ cdk/ga_app.py
@@ -8,10 +8,10 @@ from aws_cdk import (
 import os
 
 
-class BonjourFargate(core.Stack):
+class nlb_fargate_stack(core.Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+    def __init__(self, scope: core.Construct, id: str,**kwargs) -> None:
+        super().__init__(scope, id,**kwargs)
 
         # Create VPC and Fargate Cluster
         # NOTE: Limit AZs to avoid reaching resource quotas
@@ -25,7 +25,7 @@ class BonjourFargate(core.Stack):
             vpc=vpc
         )
 
-        fargate_service = ecs_patterns.NetworkLoadBalancedFargateService(
+        self.fargate_service = ecs_patterns.NetworkLoadBalancedFargateService(
             self, "FargateService",
             cluster=cluster,
             public_load_balancer = True,
@@ -34,27 +34,36 @@ class BonjourFargate(core.Stack):
             }
         )
 
-        fargate_service.service.connections.security_groups[0].add_ingress_rule(
+        self.fargate_service.service.connections.security_groups[0].add_ingress_rule(
             peer = ec2.Peer.ipv4(vpc.vpc_cidr_block),
             connection = ec2.Port.tcp(80),
             description="Allow http inbound from VPC"
         )
         
+class ga_stack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str,**kwargs) -> None:
         
+        super().__init__(scope, id,**kwargs)
         accelerator = globalaccelerator.Accelerator(self, "Accelerator")
         listener1 = globalaccelerator.Listener(self, "Listener",accelerator = accelerator, port_ranges = [{"fromPort": 80,"toPort": 80}])
         
         endpoint_group = globalaccelerator.EndpointGroup(self, "Group", listener=listener1)
-        endpoint_group.add_load_balancer("NlbEndpoint", fargate_service.load_balancer)
+        endpoint_group.add_load_balancer("NlbEndpoint", my_nlb_fargate.fargate_service.load_balancer)
 
 
         core.CfnOutput(
             self, "LoadBalancerDNS",
-            value=fargate_service.load_balancer.load_balancer_dns_name
+            value=accelerator.dns_name
         ) 
 
 app = core.App()
-BonjourFargate(app, "ga-hls-default", env=core.Environment(
+my_env = core.Environment(
     account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
-    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"])))
+    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]))
+    
+my_nlb_fargate = nlb_fargate_stack(app, "hls-default", env= my_env)
+print(my_nlb_fargate)
+my_ga = ga_stack(app, "ga-default", env= my_env)
+
+
 app.synth()
