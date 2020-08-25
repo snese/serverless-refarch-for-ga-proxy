f594920544c1e08ab7aada86483bc0d5dd76dc67
hhh2012aa <38385178+hhh2012aa@users.noreply.github.com>
2020-08-25 17:50:24 +0800

    Update ga_app.py

--- cdk/ga_app.py
+++ cdk/ga_app.py
@@ -7,6 +7,10 @@ from aws_cdk import (
 )
 import os
 import click
+from aws_cdk.aws_ecr_assets import DockerImageAsset
+import docker
+
+
 
 
 class nlb_fargate_stack(core.Stack):
@@ -25,15 +29,19 @@ class nlb_fargate_stack(core.Stack):
             self, 'Ec2Cluster',
             vpc=vpc
         )
-
+        
+        
+        #asset = DockerImageAsset(self, "MyBuildImage",directory = os.path.join("docker_folder", "my-image"))
+        
         self.fargate_service = ecs_patterns.NetworkLoadBalancedFargateService(
             self, "FargateService",
             cluster=cluster,
             public_load_balancer = True,
             task_image_options={
-                'image': ecs.ContainerImage.from_asset("../docker_folder")
+                'image': ecs.ContainerImage.from_registry("hhh2012aa/hls-nginx")
             }
         )
+        
 
         self.fargate_service.service.connections.security_groups[0].add_ingress_rule(
             peer = ec2.Peer.ipv4(vpc.vpc_cidr_block),
@@ -51,6 +59,11 @@ class nlb_fargate_stack(core.Stack):
             scale_out_cooldown=core.Duration.seconds(60),
         )
         
+        #self.fargate_service.load_balancer.add_listener('listener', port = 443)
+        self.fargate_service.listener.port = 443
+        self.fargate_service.listener.certificates = ""
+        print()
+        
 class ga_stack(core.Stack):
     def __init__(self, scope: core.Construct, id: str,**kwargs) -> None:
         
@@ -58,10 +71,10 @@ class ga_stack(core.Stack):
         
         my_nlb_fargate = nlb_fargate_stack(app, "hls-default", env= my_env)
         accelerator = globalaccelerator.Accelerator(self, "Accelerator")
-        listener1 = globalaccelerator.Listener(self, "Listener",accelerator = accelerator, 
+        my_listener = globalaccelerator.Listener(self, "Listener",accelerator = accelerator, 
         port_ranges = [{"fromPort": 80,"toPort": 80},{"fromPort": 443,"toPort": 443}])
         
-        endpoint_group = globalaccelerator.EndpointGroup(self, "Group", listener=listener1)
+        endpoint_group = globalaccelerator.EndpointGroup(self, "Group", listener=my_listener)
         endpoint_group.add_load_balancer("NlbEndpoint", my_nlb_fargate.fargate_service.load_balancer)
 
 
@@ -71,14 +84,48 @@ class ga_stack(core.Stack):
         ) 
 
 
+def create_conf(ivsurl):
+    f = open("nginx.conf", "w+")
+    f.write(
+    """
+    events {
+      worker_connections  1024;
+    }
+    
+    http{
+      default_type application/octet-stream;
+      server {
+        listen 80;
+        server_name localhost;
+        location  / {
+          proxy_pass %s;
+         }
+    }
+        types {
+          application/vnd.apple.mpegurl m3u8;
+        }
+    }
+    """
+    % (ivsurl))
+    
+    client = docker.from_env()
+    client.login(username = <yourid>, password = <yourpassword>)
+    client.images.build(path = "./docker_folder",tag = "<yourid>/hls-nginx") 
+    client.images.push('<yourid>/hls-nginx')
+
+
 
 if __name__ == '__main__':
     
     app = core.App()
+    
+    IVSURL = app.node.try_get_context("IVSURL")
+    create_conf(IVSURL)
+
     my_env = core.Environment(
         account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
         region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]))
         
     
-    my_ga = ga_stack(app, "ga-default", env= my_env)
+    my_ga = ga_stack(app, "ga-default", env= my_env) 
     app.synth()
